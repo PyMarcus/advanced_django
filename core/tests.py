046b90ef34562b63f2ab6ab825f81de230be22f3
MarcusV <Marcus-v@outlook.com>
2023-03-15 03:37:44 -0300

    review simple test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def add_num(num):
+    return num + 1
+
+
+class Simple(TestCase):
+    def setUp(self):
+        print('started')
+
+    def test_add_num(self):
+        value = add_num(3)
+        self.assertTrue(value == 4)
