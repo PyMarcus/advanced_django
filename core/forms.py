8ffb6a5a88953c7122061dd6713f69fa2040957f
MarcusV <Marcus-v@outlook.com>
2023-03-15 02:01:29 -0300

    create alerts and forms

---
+++ core/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label="Name", max_length=100)
+    email = forms.EmailField(label="Email", max_length=100)
+    subject = forms.CharField(label="Subject", max_length=100)
+    message = forms.CharField(label="Message", widget=forms.Textarea())
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        subject = self.cleaned_data['subject']
+        message = self.cleaned_data['message']
+
+        content = f"Name {name}\n{email}\n{subject}\n{message}"
+        print(content)
+        mail = EmailMessage(
+            subject=subject,
+            body=content,
+            from_email='contact@gmail.com',
+            to=['me@gmail.com'],
+            headers={'Reply-To': email}
+        )
+        mail.send()
--- core/views.py
+++ core/views.py
@@ -1,12 +1,26 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from .models import Services, Team
+from .forms import ContactForm
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name: str = 'index.html'
+    form_class: str = ContactForm
+    success_url: str = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['services'] = Services.objects.all()
         context['team'] = Team.objects.all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, "Email has been successfully sended!")
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, "Error to send e-mail")
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
